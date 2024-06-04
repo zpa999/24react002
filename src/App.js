a63f39fee9426b98f5fc8eef09623e71506eeb46
zpa009 <zpa009@nate.com>
2024-06-04 11:21:38 +0900

    2406041121

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import About from "./routes/About";
 import "./App.css";
-import { HashRouter, Route } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return;
+  return (
+    <div className="about__container">
+      <span>This is My ReactApp</span>
+    </div>
+  );
 }
 
 export default About;
