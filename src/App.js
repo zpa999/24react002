081539626e3acd6d2f45810a88a68210694d2a6f
zpa999 <zpa009@nate.com>
2025-02-28 09:21:02 +0900

    2502280920

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <HashRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-    </HashRouter>
+    <div>
+      <span>hello world</span>
+    </div>
   );
 }
 
